1270e975e2cac36caf601a700292026787cfe9fe
Allie Soldau <alliesoldau@gmail.com>
2023-07-24 18:10:31 -0400

    created sqlite db for the backend

--- create_database.py
+++ create_database.py
@@ -1,5 +1,6 @@
 import sqlite3
 import datetime
+import json
 
 # create and connect to SQL database
 connection = sqlite3.connect('sales_data.sqlite')
@@ -11,19 +12,38 @@ cursor.execute("DROP TABLE if exists books");
 # create the books table 
 cursor.execute("""CREATE Table if not exists books
                 (
-                    id int primary_key, 
-                    author string,
-                    title string,
-                    date_completed datetime
-                    publication_year int,
-                    publisher string,
-                    publisher_address string,
-                    url string,
-                    rating float,
-                    page_count int,
-                    genres string
+                    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    author STRING,
+                    title STRING,
+                    date_completed DATETIME,
+                    publisher STRING,
+                    publisher_address STRING,
+                    url STRING,
+                    rating FLOAT,
+                    page_count INT,
+                    genres STRING
                 )
                   """)
+
+# open JSON file
+f = open('db.json')
+  
+# returns JSON object as a dictionary
+books = json.load(f)
+  
+# iterate through the json list
+for book in books:
+    genres_str = ', '.join(book['genres'])
+    book_data = (book['author'], book['title'], book['date_completed'], book['publisher'],
+                 book['publisher_address'], book['url'], book['rating'], book['page_count'], genres_str)
+    cursor.execute("""INSERT INTO books
+                    (author, title, date_completed, publisher, publisher_address, url, rating, page_count, genres)
+                   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+                    """, book_data)
+  
+# close file 
+f.close()
+
 
 connection.commit()
 connection.close()
